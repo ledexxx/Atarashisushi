a3d9e1e895f1734046fe6717baace0c449ba877e
ledexxx <146101070+ledexxx@users.noreply.github.com>
2024-09-21 00:51:11 -0300

    0.1.1

--- menu_marco/src/Menu.jsx
+++ menu_marco/src/Menu.jsx
@@ -1,80 +1,215 @@
 import React, { useState } from 'react';
 import { Search } from '@mui/icons-material';
-import { Restaurant, LocalDining, ShoppingBasket, EmojiFoodBeverage, Cake, Liquor} from '@mui/icons-material'; 
+import { Restaurant, LocalDining, ShoppingBasket, LunchDining , Cake, Liquor} from '@mui/icons-material'; 
 import './App.css';
 import Logo from './logo';
 import Slider from "react-slick";
 import { FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa';
+import { BiSushi } from "react-icons/bi";
+import { TbMoodKid } from "react-icons/tb";
+import { RiDiscountPercentLine } from "react-icons/ri";
+import { GiSushis } from "react-icons/gi";
+
 
 const Menu = () => {
+  const iconSize = 24; 
+
   const categories = [
-    { name: 'Todo', icon: <ShoppingBasket /> },
-    { name: 'Ensaladas', icon: <LocalDining /> },
-    { name: 'Sushi Rolls', icon: <Restaurant /> },
-    { name: 'Postres', icon: <Cake /> },
-    { name: 'Bebidas', icon: <EmojiFoodBeverage /> },
-    { name: 'Licores', icon: <Liquor /> },
+    { name: 'Todo', icon: <ShoppingBasket size={iconSize} /> },
+    { name: 'Promociones', icon: <RiDiscountPercentLine  size={iconSize} /> },
+    { name: 'Sushi Rolls', icon: <GiSushis  size={iconSize} /> },
+    { name: 'Handrolls Clásicos', icon: <BiSushi size={iconSize} /> },
+    { name: 'Sushi Burgers', icon: <LunchDining size={iconSize} /> },
+    { name: 'Niños', icon: <TbMoodKid size={iconSize} /> },
+    { name: 'Bebidas', icon: <Liquor size={iconSize} /> },
+  
   ];
+
+
   // Estado para la categoría seleccionada y el término de búsqueda
   const [selectedCategory, setSelectedCategory] = useState('Todo');
   const [searchTerm, setSearchTerm] = useState('');
   
 
   const products = [
-     // Ensaladas
+      // Menú de Niños
+    { 
+      name: "Nuggets con Papas", 
+      description: "Nuggets de pollo acompañados con papas fritas", 
+      price: "$4.990", 
+      image: "/niños.jpeg",
+      category: "Niños"
+  },
+  { 
+      name: "Tiritas con Papas", 
+      description: "Tiritas de pollo acompañadas con papas fritas", 
+      price: "$5.990", 
+      image: "/niños.jpeg",
+      category: "Niños"
+  },
+        // Handrolls Clásicos
+      { 
+        name: "Handroll Pollo Cebollín", 
+        description: "Pollo con cebollín envuelto en panko o tempura", 
+        price: "$3.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls Clásicos"
+    },
+    { 
+        name: "Handroll Pollo Aceituna", 
+        description: "Pollo con aceituna envuelto en panko o tempura", 
+        price: "$3.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls Clásicos"
+    },
+    { 
+        name: "Handroll Kanikama Cebollín", 
+        description: "Kanikama con cebollín envuelto en panko o tempura", 
+        price: "$3.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls Clásicos"
+    },
+    { 
+        name: "Handroll Kanikama Aceituna", 
+        description: "Kanikama con aceituna envuelto en panko o tempura", 
+        price: "$3.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls Clásicos"
+    },
+
+    // Handrolls Premium
+    { 
+        name: "Handroll Pollo + Verdura a elección", 
+        description: "Pollo y verdura a elección envuelto en panko o tempura", 
+        price: "$4.490", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls Premium"
+    },
+    { 
+        name: "Handroll Kanikama + Verdura a elección", 
+        description: "Kanikama y verdura a elección envuelto en panko o tempura", 
+        price: "$4.490", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls Premium"
+    },
+
+    // Handrolls VIP
+    { 
+        name: "Handroll Carne + Verdura a elección", 
+        description: "Carne y verdura a elección envuelto en panko o tempura", 
+        price: "$4.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls VIP"
+    },
+    { 
+        name: "Handroll Cerdo + Verdura a elección", 
+        description: "Cerdo y verdura a elección envuelto en panko o tempura", 
+        price: "$4.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls VIP"
+    },
+    { 
+        name: "Handroll Camarón + Verdura a elección", 
+        description: "Camarón y verdura a elección envuelto en panko o tempura", 
+        price: "$4.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls VIP"
+    },
+    { 
+        name: "Handroll Salmón + Verdura a elección", 
+        description: "Salmón y verdura a elección envuelto en panko o tempura", 
+        price: "$4.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Handrolls VIP"
+    },
+        // Gohan
+      { 
+        name: "Gohan Opción 1", 
+        description: "Pollo apanado, arroz, queso, cebollín, palta", 
+        price: "$4.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Gohan"
+    },
+    { 
+        name: "Gohan Opción 2", 
+        description: "Camarón apanado, arroz, queso, lechuga, cebollín, palta", 
+        price: "$5.490", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Gohan"
+    },
+    { 
+        name: "Gohan Opción 3", 
+        description: "Salmón, arroz, queso, lechuga, cebollín, palta", 
+        price: "$5.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Gohan"
+    },
+    { 
+        name: "Gohan Opción 4", 
+        description: "Arroz, queso, palta, lechuga, pimentón, aceituna, choclo, cebollín", 
+        price: "$4.990", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Gohan"
+    },
+    { 
+        name: "Gohan Opción 5", 
+        description: "Camarón, salmón, arroz, lechuga, palta, queso", 
+        price: "$6.490", 
+        image: "URL_DE_LA_IMAGEN",
+        category: "Gohan"
+    },
    
-     { 
-      name: "Ensalada César", 
-      description: "Lechuga romana, pollo a la parrilla, crotones y aderezo César", 
-      price: "$8.500", 
-      image: "https://www.gourmet.cl/wp-content/uploads/2016/09/Ensalada_C%C3%A9sar-web.jpg" 
-      ,category: "Ensaladas"
-  },
-
- 
-  { 
-      name: "Ensalada de Quinoa", 
-      description: "Quinoa, aguacate, espinacas, y nueces", 
-      price: "$10.000", 
-      image: "https://danzadefogones.com/wp-content/uploads/2024/08/ensalada-de-quinoa-facil.jpg" ,
-      category: "Ensaladas"
-  },
-  { 
-      name: "Ensalada Waldorf", 
-      description: "Manzana, apio, nueces y mayonesa ligera", 
-      price: "$11.300", 
-      image: "https://www.gourmet.cl/wp-content/uploads/2023/11/WALDORF.jpg" ,
-      category: "Ensaladas"
-  },
-  { 
-      name: "Ensalada de Atún", 
-      description: "Atún, huevo duro, tomate cherry y lechuga", 
-      price: "$9.800", 
-      image: "https://www.gourmet.cl/wp-content/uploads/2021/10/ensalada-de-at%C3%BAn-internet-1.jpg",
-      category: "Ensaladas"
-  },
-
-    { 
-      name: "Ensalada Caprese", 
-      description: "Tomate, mozzarella fresca, albahaca y aceite de oliva", 
-      price: "$9.200", 
-      image: "https://i.blogs.es/2d572b/caprese/1366_2000.jpg",
-      category: "Ensaladas"
+        // Sushi Promociones
+      { 
+        name: "Sushi Promo 20 cortes", 
+        description: "Incluye 1 roll frío, 2 calientes y 1 premium", 
+        price: "$10.990", 
+        image: "/combo20.jpeg",
+        category: "Promociones"
     },
     { 
-      name: "Ensalada Griega", 
-      description: "Pepino, tomate, aceitunas, cebolla roja y queso feta", 
-      price: "$7.900", 
-      image: "https://images.getrecipekit.com/20211227205902-ensalada-20griega.jpg?width=650&quality=90&",
-      category: "Ensaladas"
+        name: "Sushi Promo 30 cortes", 
+        description: "Incluye 1 roll frío, 3 calientes y 1 premium", 
+        price: "$12.990", 
+        image: "/combo30.jpeg",
+        category: "Promociones"
     },
+    { 
+        name: "Sushi Promo 40 cortes", 
+        description: "Incluye 2 rolls fríos, 3 calientes y 1 premium", 
+        price: "$14.990", 
+        image: "/combo40.jpeg",
+        category: "Promociones"
+    },
+    { 
+        name: "Sushi Promo 50 cortes", 
+        description: "Incluye 2 rolls fríos, 4 calientes y 1 premium", 
+        price: "$16.990", 
+        image: "/combo40.jpeg",
+        category: "Promociones"
+    },
+    { 
+        name: "Sushi Promo 60 cortes", 
+        description: "Incluye 2 rolls fríos, 4 calientes y 1 premium", 
+        price: "$18.990", 
+        image: "/combo40.jpeg",
+        category: "Promociones"
+    },
+    { 
+        name: "Sushi Promo 70 cortes", 
+        description: "Incluye 2 rolls fríos, 5 calientes y 1 premium", 
+        price: "$20.990", 
+        image: "/combo40.jpeg",
+        category: "Promociones"
+    },
+   
     // Sushi Rolls
     
     { 
       name: 'Sushi California', 
       description: 'Salmón, aguacate, y queso crema', 
       price: '$8.800', 
-      image: 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/0749D9BC-260D-40F4-A07F-54814C4A82B4/Derivates/A73A7793-F3EE-4B90-ABA4-1CC1A0C3E18F.jpg',
+      image: '/California_roll.jpeg',
       category: 'Sushi Rolls'
     },
     { 
@@ -134,21 +269,6 @@ const Menu = () => {
       category: 'Sushi Rolls'
     },
    
-    // Postres
-    { 
-      name: 'Tiramisú', 
-      description: 'Postre italiano con mascarpone, café y cacao', 
-      price: '$6.500', 
-      image: 'https://www.flavoursholidays.co.uk/wp-content/uploads/2020/07/Tiramisu.jpg',
-      category: 'Postres'
-    },
-    { 
-      name: 'Cheesecake de Frutos Rojos', 
-      description: 'Pastel de queso con una base de galleta y cobertura de frutos rojos', 
-      price: '$7.200', 
-      image: 'https://mandolina.co/wp-content/uploads/2024/04/Cheesecake-de-Frutos-Rojos-1080x550-1.jpg',
-      category: 'Postres'
-    },
     // Bebidas
     { 
       name: 'Té Matcha', 
@@ -164,21 +284,7 @@ const Menu = () => {
       image: 'https://mejorconsalud.as.com/wp-content/uploads/2020/12/limonada-coco.jpg',
       category: 'Bebidas'
     },
-    // Licores
-    { 
-      name: 'Sake', 
-      description: 'Bebida alcohólica japonesa elaborada a partir de arroz fermentado', 
-      price: '$12.000', 
-      image: 'https://www.justonecookbook.com/wp-content/uploads/2019/09/Sake-II.jpg',
-      category: 'Licores'
-    },
-    { 
-      name: 'Mojito', 
-      description: 'Cóctel cubano con ron, menta y lima', 
-      price: '$9.500', 
-      image: 'https://cookieandkate.com/images/2020/08/best-mojito-recipe-2.jpg',
-      category: 'Licores'
-    }
+   
   ];
 
   const filteredProducts = products.filter(product => {
@@ -204,7 +310,7 @@ const Menu = () => {
     if (selectedCategory === 'Todo') {
       return Object.keys(groupedProducts).map((category, index) => (
         <div key={index} className="my-4">
-          <h2 className="text-2xl font-bold mb-4">{category}</h2>
+          <h2 className="text-2xl font-bold mb-4 text-[#ff5f24]">{category}</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {groupedProducts[category].map((product, idx) => (
               <ProductCard key={idx} product={product} />
@@ -234,35 +340,31 @@ const Menu = () => {
 
   return (
     <>
- <header className="flex justify-between items-center max-w-5xl mx-auto px-4 py-2 bg-transparent">
-  <div className="text-white cursor-pointer">
-    {/* Aquí puedes usar un icono de menú que prefieras */}
-    <Search className="text-[#591f2a]" fontSize="large" />
-  </div>
-  <div className="mx-auto">
-    <Logo />
-  </div>
-  <div className="text-white cursor-pointer">
-    <ShoppingBasket className="text-[#591f2a]" fontSize="large" />
-  </div>
-</header>
-
-
+      <header className="flex justify-between items-center max-w-5xl mx-auto px-4 py-2 bg-transparent">
+        <div className="text-white cursor-pointer">
+          <Search className="text-[#ff5f24]" fontSize="large" />
+        </div>
+        <div className="mx-auto">
+          <Logo />
+        </div>
+        <div className="text-white cursor-pointer">
+          <ShoppingBasket className="text-[#ff5f24]" fontSize="large" />
+        </div>
+      </header>
 
       <div className="container mx-auto px-4">
-      <Slider {...settings}>
-  <div className="max-w-[70%] h-[250px] mx-auto rounded-lg overflow-hidden">
-    <img src="/slider1.jpeg" alt="slide1" className="w-full h-full object-cover" />
-  </div>
-  <div className="max-w-[70%] h-[200px] mx-auto rounded-lg overflow-hidden">
-    <img src="/slider2.jpeg" alt="slide2" className="w-full h-full object-cover" />
-  </div>
-  <div className="max-w-[70%] h-[200px] mx-auto rounded-lg overflow-hidden">
-    <img src="/slider3.jpeg" alt="slide3" className="w-full h-full object-cover" />
-  </div>
-</Slider>
+        <Slider {...settings}>
+          <div className="max-w-[70%] h-[250px] mx-auto rounded-lg overflow-hidden">
+            <img src="/slider1.jpeg" alt="slide1" className="w-full h-full object-cover" />
+          </div>
+          <div className="max-w-[70%] h-[200px] mx-auto rounded-lg overflow-hidden">
+            <img src="/slider2.jpeg" alt="slide2" className="w-full h-full object-cover" />
+          </div>
+          <div className="max-w-[70%] h-[200px] mx-auto rounded-lg overflow-hidden">
+            <img src="/slider3.jpeg" alt="slide3" className="w-full h-full object-cover" />
+          </div>
+        </Slider>
 
-        
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <CategorySelector categories={categories} selectedCategory={selectedCategory} onSelectCategory={setSelectedCategory} />
         {renderProducts()}
@@ -275,19 +377,19 @@ const Menu = () => {
               <img src="/logosushi.svg" className="h-28" alt="sushi Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap text-white"></span>
             </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-[#591f2a] sm:mb-0">
               <li className="mr-4 md:mr-6">
-                <a href="https://www.tiktok.com/@atarashisushi?lang=en" className="hover:text-[#591f2a]">
+                <a href="https://www.tiktok.com/@atarashisushi?lang=en" className="hover:text-[#ff5f24]">
                   <FaTiktok size={24} />
                 </a>
               </li>
               <li className="mr-4 md:mr-6">
-                <a href="https://m.facebook.com/atarashiisushi/albums/509553727840026/" className="hover:text-[#591f2a]">
+                <a href="https://m.facebook.com/atarashiisushi/albums/509553727840026/" className="hover:text-[#ff5f24]">
                   <FaFacebookF size={24} />
                 </a>
               </li>
               <li className="mr-4 md:mr-6">
-                <a href="https://www.instagram.com/atarashisushii/" className="hover:text-[#591f2a]">
+                <a href="https://www.instagram.com/atarashisushii/" className="hover:text-[#ff5f24]">
                   <FaInstagram size={24} />
                 </a>
               </li>
@@ -302,7 +404,7 @@ const Menu = () => {
 };
 
 const ProductCard = ({ product }) => (
-  <div className="flex items-center bg-[#0000] border-2 border-red-900 rounded-xl shadow-md overflow-hidden w-full">
+  <div className="flex items-center bg-[#0000] border-2 border-[#591f2a] rounded-xl hover:bg-[#ff7b4b7e] shadow-md overflow-hidden w-full">
     <div className="w-1/3">
       <img src={product.image} alt={product.name} className="w-full h-full object-cover" />
     </div>
@@ -310,7 +412,7 @@ const ProductCard = ({ product }) => (
       <h2 className="text-xl text-left font-bold text-white">{product.name}</h2>
       <p className="text-white text-left">{product.description}</p>
       <div className="flex items-center mt-4">
-        <ShoppingBasket className="text-[#591f2a] mr-2" />
+        <ShoppingBasket className="text-[#591f1f] mr-2" />
         <span className="bg-[#591f2a] text-[#fadada] font-bold py-1 px-3 rounded-lg">
           {product.price}
         </span>
@@ -327,7 +429,7 @@ const SearchBar = ({ searchTerm, setSearchTerm }) => (
         placeholder="Buscar productos por nombre o descripción"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-full p-2 bg-transparent border border-red-900 rounded-full focus:outline-none"
+        className="w-full p-2 bg-transparent border border-[#591f2a] rounded-full focus:outline-none"
       />
       <Search className="absolute right-4 top-3 text-white" />
     </div>
@@ -343,7 +445,7 @@ const CategorySelector = ({ categories, selectedCategory, onSelectCategory }) =>
           className="text-center cursor-pointer" 
           onClick={() => onSelectCategory(category.name)}
         >
-          <div className={`rounded-full p-4 mb-2 ${selectedCategory === category.name ? 'bg-[#591f2a]' : 'bg-[#421820]'}`}>
+          <div className={`rounded-full w-16 h-16 flex items-center justify-center mx-5 mb-2 ${selectedCategory === category.name ? 'bg-[#ff5f24]' : 'bg-[#421820]'}`}>
             {category.icon}
           </div>
           <p>{category.name}</p>
